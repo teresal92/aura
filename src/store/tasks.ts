53abae3b35e5773342a9a4370892b10e25f53cd1
Teresa Lew <teresalew92@gmail.com>
2026-03-02 14:56:50 -0800

    chore: use crypto.randomUUID for task ids

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -17,6 +17,9 @@ interface TaskState {
 }
 
 function generateId(): string {
+  if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
+    return crypto.randomUUID()
+  }
   return Math.random().toString(36).substring(2, 15) + Date.now().toString(36)
 }
 
